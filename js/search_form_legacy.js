e7c380278cc5527da5a41615bb1350f69c9175b2
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2021-01-01 18:49:49 -0500

    update

---
+++ js/search_form_legacy.js
@@ -0,0 +1,8 @@
+var results_location = "/OpenSearch/search_legacy.html";
+
+function search_form(jse_Form) {
+	if (jse_Form.d.value.length > 0) {
+		document.cookie = "q=" + escape(jse_Form.d.value);
+		window.location = results_location;
+	}
+}
