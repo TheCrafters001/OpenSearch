027d14b6e2e6c0fa55a0eb881cc25fa059280f05
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2021-11-09 15:20:20 -0500

    complete overhaul on Search Engine

---
+++ search/tipuesearch_content.js
@@ -0,0 +1,43 @@
+
+var tipuesearch = {"pages": [
+     /* Main Google Services */
+     {"title": "Google", "text": "Google Search Engine.", "tags": "google search engine", "url": "https://google.com/"},
+     {"title": "GMail", "text": "GMail - Google Email.", "tags": "google gmail email mail", "url": "https://gmail.com/"},
+     {"title": "Google Drive", "text": "Google Drive - Google Cloud Storage.", "tags": "google cloud drive storage", "url": "https://drive.google.com/"},
+     {"title": "Google Docs", "text": "Google Docs", "tags": "google docs cloud drive storage.", "url": "https://docs.google.com/"},
+     {"title": "Google Forms", "text": "Google Forms.", "tags": "google docs forms cloud drive storage", "url": "https://docs.google.com/forms/"},
+     {"title": "Google Classroom", "text": "Google Classroom.", "tags": "google docs forms cloud drive storage classroom", "url": "https://classroom.google.com/"},
+     {"title": "Google Fonts", "text": "Google Fonts.", "tags": "google fonts", "url": "https://fonts.google.com/"},
+     {"title": "Google Translate", "text": "Google Translate.", "tags": "google translate", "url": "https://translate.google.com/"},
+     {"title": "YouTube", "text": "Broadcast Yourself.", "tags": "google youtube videos", "url": "https://youtube.com/"},
+     {"title": "YouTube Music", "text": "Broadcast Yourself.", "tags": "google youtube videos music", "url": "https://music.youtube.com/"},
+     {"title": "Blogger", "text": "Create a unique and beautiful blog. It’s easy and free.", "tags": "google blog videos docs drive", "url": "https://www.blogger.com/"},
+     {"title": "Firebase", "text": "Firebase helps mobile and web app teams succeed.", "tags": "google fireside developer", "url": "https://firebase.google.com/"},
+     /* End Main Google Services */
+     /* Game Sites */
+     {"title": "Game Jolt", "text": "Games for the love of it.", "tags": "gamejolt games indie free paid marketplace", "url": "https://gamejolt.com/"},
+     {"title": "Itch", "text": "Download the latest indie games - itch.io.", "tags": "itch itch.io games indie free paid marketplace", "url": "https://itch.io/"},
+     {"title": "IndieDB", "text": "Rockin' Indie Games one pixel at a time - Indie DB.", "tags": "indiedb db games indie free paid marketplace", "url": "https://indiedb.com/"},
+     {"title": "Welcome to Steam", "text": "Steam is the ultimate destination for playing, discussing, and creating games.", "tags": "steam games software marketplace valve workshop", "url": "https://store.steampowered.com"},
+     {"title": "Origin", "text": "Get great PC and Mac games on Origin. Play the latest RPGs, shooters, Sims games & more. Try before you buy demos and trials and score total free games!", "tags": "origin games market", "url": "https://origin.com/"},
+     {"title": "ROBLOX", "text": "Roblox is ushering in the next generation of entertainment. Imagine, create, and play together with millions of players across an infinite variety of immersive, user-generated 3D worlds. Roblox is a global platform that brings people together through play.", "tags": "roblox games market user-generated ugc experience", "url": "https://roblox.com/"},
+     {"title": "Relic Castle", "text": "Relic Castle is a tight-knit community for Pokemon fan games. We have something to offer players and developers, both old and new.", "tags": "pokemon games software resources developer", "url": "https://reliccastle.com/"},
+     {"title": "Essentials.TF", "text": "Competitive Team Fortress 2 Coverage &amp; Casting.", "tags": "team fortress competitive tf2 games casting hosting", "url": "https://essentials.tf/"},
+     /* End Game Sites */
+     /* Search Engines */
+     {"title": "DuckDuckGo", "text": "DuckDuckGo. Privacy, simplified.", "tags": "duckduckgo duck search engine", "url": "https://duckduckgo.com/"},
+     {"title": "OpenSearch", "text": "OpenSearch. A free search engine, keeping your data, yours.", "tags": "search engine thecrafters001 github open-source free", "url": "https://thecrafters001.github.io/OpenSearch"},
+     /* End Search Engines */
+     /* Open-Source */
+     {"title": "TheCrafters001", "text": "TheCrafters001 - Redefining Free.", "tags": "software games free open-source pokemon thecrafters001 fangames fan-games", "url": "https://thecrafters001.github.io/"},
+     {"title": "GitHub", "text": "The OpenSource Hub.", "tags": "software games free open-source github", "url": "https://github.com/"},
+     /* End Open-Source */
+     /* Twitter */
+     {"title": "Twitter", "text": "I would like to put a description here, but the site won't let me.", "tags": "twitter social media tweet retweet networking", "url": "https://twitter.com/"},
+     {"title": "TweetDeck", "text": "I would like to put a description here, but the site won't let me.", "tags": "twitter social media tweet retweet networking tweetdeck", "url": "https://tweetdeck.twitter.com/"},
+     /* End Twitter */
+     /* Microsoft */
+     {"title": "Visual Studio: IDE and Code Editor for Software Developers and Teams", "text": "Visual Studio dev tools & services make app development easy for any developer, on any platform & language. Develop with our code editor or IDE anywhere for free.", "tags": "development visual studio 2019 2022 2017 2015 2013 code", "url": "https://visualstudio.com/"},
+     {"title": "Visual Studio Code - Code Editing. Redefined", "text": "Visual Studio Code is a code editor redefined and optimized for building and debugging modern web and cloud applications. Visual Studio Code is free and available on your favorite platform - Linux, macOS, and Windows.", "tags": "development visual studio 2019 2022 2017 2015 2013 code", "url": "https://code.visualstudio.com/"},
+     /* End Microsoft */
+]};
